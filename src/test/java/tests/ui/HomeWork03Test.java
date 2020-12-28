f1818187db87148c9ae71cfcc572382ebcdc27d7
BreenBB <breen@ukr.net>
2020-12-28 21:00:41 +0200

    Test

--- src/test/java/tests/ui/HomeWork03Test.java
+++ src/test/java/tests/ui/HomeWork03Test.java
@@ -3,7 +3,6 @@ package tests.ui;
 import io.qameta.allure.*;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
