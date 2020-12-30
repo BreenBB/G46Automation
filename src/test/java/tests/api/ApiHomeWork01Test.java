3b756dab09166bc4c33b6e4573730906ef09254f
BreenBB <breen@ukr.net>
2020-12-30 11:19:16 +0200

    Test

--- src/test/java/tests/api/ApiHomeWork01Test.java
+++ src/test/java/tests/api/ApiHomeWork01Test.java
@@ -11,9 +11,10 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.hasItems;
 
-public class ApiHomeWork01Test extends BaseApiTest {
+public class ApiHomeWork01Test {
 
     private final static Logger LOG = LogManager.getLogger("API Тест получение списка банкоматов");
+    private String city="Киев";
 
     @Owner("BreenBB")
     @Feature("")
@@ -24,29 +25,26 @@ public class ApiHomeWork01Test extends BaseApiTest {
     @Severity(SeverityLevel.CRITICAL)
     @Test
     public void checkATMList() {
-        //List<String> result =
-                given()
-                .spec(this.reqspec)
+        List<String> result = given()
                 .queryParam("json")
                 .queryParam("atm")
                 .queryParam("address", "")
-                .queryParam("city", "Киев")
+                .queryParam("city", city)
                 .when()
                 .get("https://api.privatbank.ua/p24api/infrastructure")
                 .then()
-                .spec(this.resSpec)
                 .assertThat()
                 .body("devices.type", hasItems(equalTo("ATM")))
                 .body("devices.cityRU", hasItems(equalTo("Киев")))
-                .body("devices.cityEN", hasItems(equalTo("Kyiv")));
-                //.extract()
-                //.jsonPath()
-                //.getList("devices.fullAddressRu");
+                .body("devices.cityEN", hasItems(equalTo("Kyiv")))
+                .extract()
+                .jsonPath()
+                .getList("devices.fullAddressRu");
 
-        /*//Вывод полученных адресов банкоматов
+        //Вывод полученных адресов банкоматов
         LOG.info("Адреса Банкоматов: ");
         for(int i = 0; i < result.size(); i++) {
             LOG.info(result.get(i));
-        }*/
+        }
     }
 }
